44b9ca82c6baa1152e9ed4a7ada5df0f248ac6c8
fomes <contato.felipeluna@gmail.com>
2020-10-27 19:28:35 -0300

    change initial page pokedex

--- pokedex/js/script.js
+++ pokedex/js/script.js
@@ -1,14 +1,7 @@
 const root = document.querySelector('#root');
 const form = document.querySelector('#myForm');
 const qtd = document.querySelector('#qtd');
-let pokemon, desc;
-
-const getDesc = async (n) => {
-  let resp = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${n}`);
-  let data = await resp.json();
-  desc = data.flavor_text_entries[0].flavor_text;
-  console.log(data.flavor_text_entries[0])
-}
+let pokemon;
 
 const get = async (n) => {
   let resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${n}`);
@@ -22,22 +15,23 @@ const fetchList = async (n) => {
     document.querySelector('.card').remove();
   }
 
-  await get(n);
-  await getDesc(n);
+  for (i=1; i<=n; i++) {
+    await get(i);
 
-  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-  const div = document.createElement('div');
-  root.appendChild(div);
-  div.classList.add('card');
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const div = document.createElement('div');
+    root.appendChild(div).classList.add('card');
+  
+    div.innerHTML =
+      `<img class='card-header' src='${pokemon.sprites.front_default}'>
+      <p class='card-header' id=#${pokemon.id}>#${pokemon.id.toString().padStart(3, '0')}</p>
+      <p class='name'>${name}</p>
+      <p class='type'>Type: ${pokemon.types[0].type.name}</p>`;
+  }
 
-  div.innerHTML =
-    `<img class='card-header' src='${pokemon.sprites.front_default}'>
-    <p class='card-header' id=#${pokemon.id}>#${pokemon.id.toString().padStart(3, '0')}</p>
-    <p class='name'>${name}</p>
-    <p class='type'>Type: ${pokemon.types[0].type.name}</p>
-    <p class='type'>Desc: ${desc.replace(/[^a-zA-Z. ]/g, "")}</p>
-    `;
 };
+
+fetchList(18);
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
