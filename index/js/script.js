64c75bf0a6b876e5067d49cd3e03d24f757ead6f
fomes <contato.felipeluna@gmail.com>
2021-08-26 17:11:14 -0300

    finish new homepage

---
+++ index/js/script.js
@@ -0,0 +1,11 @@
+function goToProjects() {
+  window.location.href = "http://localhost:5500/projects.html";
+
+  // window.location.href = "https://fomes.github.io/projects";
+}
+
+function goToHome() {
+  window.location.href = "http://localhost:5500/index.html";
+
+  // window.location.href= "https://fomes.github.io/";
+}
