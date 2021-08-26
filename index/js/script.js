bb75b274a06054f49fdf7a250c953e1f8b3cdc8e
fomes <contato.felipeluna@gmail.com>
2021-08-26 17:17:21 -0300

    fix routes xD

--- index/js/script.js
+++ index/js/script.js
@@ -1,11 +1,11 @@
 function goToProjects() {
-  window.location.href = "http://localhost:5500/projects.html";
+  window.location.href = "https://fomes.github.io/projects.html";
 
-  // window.location.href = "https://fomes.github.io/projects";
+  // window.location.href = "http://localhost:5500/projects.html";
 }
 
 function goToHome() {
-  window.location.href = "http://localhost:5500/index.html";
+  window.location.href= "https://fomes.github.io/";
 
-  // window.location.href= "https://fomes.github.io/";
+  // window.location.href = "http://localhost:5500/index.html";
 }
